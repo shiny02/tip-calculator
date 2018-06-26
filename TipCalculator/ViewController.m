1b965e4906675c20a17a7786040e993dfc46e7bc
Youngmin Shin <shinyoungmin@fb.com>
2018-06-26 15:07:44 -0700

    First commit

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+
 @end
 
 @implementation ViewController
@@ -25,5 +30,45 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)onTap:(id)sender {
+    NSLog(@"Hello all!");
+    [self.view endEditing:YES];
+}
+
+- (IBAction)onEdit:(id)sender {
+    
+    
+    double bill = [self.billField.text doubleValue];
+ 
+    NSArray *percentages = @[@(0.15), @(0.2), @(0.25)];
+    double tipPercent = [percentages[self.tipControl.selectedSegmentIndex] doubleValue];
+    
+    double tip = bill * tipPercent;
+    double total = bill + tip;
+    
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
+}
+
+- (IBAction)onEditingBegin:(id)sender {
+    [UIView animateWithDuration:0.2 animations:^{
+        self.billField.frame = CGRectMake(self.billField.frame.origin.x, self.billField.frame.origin.y+30, self.billField.frame.size.width, self.billField.frame.size.height);
+        self.tipLabel.alpha = 0;
+    }];
+
+}
+
+- (IBAction)onEditingEnd:(id)sender {
+    
+    CGRect newFrame = self.billField.frame;
+    newFrame.origin.y -= 30;
+    [UIView animateWithDuration:0.2 animations:^{
+        self.billField.frame = newFrame;
+        self.tipLabel.alpha = 1; 
+    }];
+  
+    
+}
 
 @end
